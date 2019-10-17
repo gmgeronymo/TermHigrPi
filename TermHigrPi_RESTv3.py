ca098fdd9d532d5c8700316b1e3f73bacf64eb9f
Gean Marcos Geronymo <gean.geronymo@gmail.com>
2019-10-17 15:35:08 +0000

    Update TermHigrPi_RESTv3.py

--- TermHigrPi_RESTv3.py
+++ TermHigrPi_RESTv3.py
@@ -175,8 +175,8 @@ if __name__ == "__main__":
          pressure = round(p,1)
       
       if (config['LCDConfig']['enable'] == 'true') :
-         lcd.put_line(0, "Temperatura: "+str(temperature)+" C")
-         lcd.put_line(1, "Umid: "+str(humidity)+" u.r.  P: "+str(pressure))
+         lcd.put_line(0, data_atual['hora'] + "    "+str(temperature)+" "+chr(223)+"C")
+         lcd.put_line(1, str(humidity)+" %u.r. "+str(pressure)+ "hPa")
          
 
          if (first_run) :
