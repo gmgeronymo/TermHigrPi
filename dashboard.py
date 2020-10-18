0b99ee1735fcb3e0be4e9d2edf26c2c5c149e087
Gean Marcos Geronymo <gean.geronymo@gmail.com>
2020-10-18 15:59:47 -0300

    grafico mes com max e min

--- dashboard.py
+++ dashboard.py
@@ -42,7 +42,8 @@ def select_last_month_data(conn):
     query = "SELECT date, temperature, humidity, pressure FROM condicoes_ambientais WHERE date >= datetime('now', '-1 month') AND date < datetime('now')" 
     df = pd.read_sql_query(query,conn,parse_dates={'date':'%Y-%m-%d %H:%M:%S'},index_col='date')
     df[['temperature','humidity','pressure']] = df[['temperature','humidity','pressure']].apply(pd.to_numeric)   
-    return df.resample('120Min', how='mean')
+    #return df.resample('120Min', how='mean')
+    return df.resample('1D').agg(['mean','min','max'])
 
 app = Flask(__name__)
 
@@ -85,11 +86,20 @@ def grafico_month():
     with conn:
         df = select_last_month_data(conn)
         ca_labels = df.index.strftime('%d/%m/%Y %H:%M:%S').tolist()
-        ca_temp = df['temperature'].tolist()
-        ca_hum = df['humidity'].tolist()
-        ca_press = df['pressure'].tolist()
-    
-    return render_template('multiple_chart.html', labels=ca_labels, temp=ca_temp, hum=ca_hum, press=ca_press)
+        
+        ca_temp = df['temperature']['mean'].tolist()
+        ca_hum = df['humidity']['mean'].tolist()
+        ca_press = df['pressure']['mean'].tolist()
+
+        ca_temp_min = df['temperature']['min'].tolist()
+        ca_hum_min = df['humidity']['min'].tolist()
+        ca_press_min = df['pressure']['min'].tolist()
+
+        ca_temp_max = df['temperature']['max'].tolist()
+        ca_hum_max = df['humidity']['max'].tolist()
+        ca_press_max = df['pressure']['max'].tolist()
+        
+    return render_template('multiple_chart_maxmin.html', labels=ca_labels, temp=ca_temp, hum=ca_hum, press=ca_press, temp_min=ca_temp_min, hum_min=ca_hum_min, press_min=ca_press_min, temp_max=ca_temp_max, hum_max=ca_hum_max, press_max=ca_press_max)
 
 @app.route('/grafico_all')
 def grafico_all():
